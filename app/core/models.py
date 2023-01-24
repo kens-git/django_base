c44d8e12460ec66852cf9a29ddae8d4e4d14b197
Ken Montgomery <>
2023-01-24 14:32:12 -0500

    Initial commit.

---
+++ app/core/models.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    def __str__(self) -> str:
+        return self.username
---
+++ app/core/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpRequest, HttpResponse
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Nothing here yet.</h1>')
