faeb9b9e4c805570555b649be1d613be831c30c3
Ken Montgomery <>
2023-01-24 20:12:39 -0500

    Added .pre-commit-config.yaml and a separate requirements file for development.

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     def __str__(self) -> str:
         return self.username
--- app/core/views.py
+++ app/core/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest, HttpResponse
 
+
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Nothing here yet.</h1>')
+    return HttpResponse("<h1>Nothing here yet.</h1>")
