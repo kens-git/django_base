d56f635c40af042208ebe8ec200764e11c17af8b
Ken Montgomery <>
2023-01-26 19:07:14 -0500

    Updated config.urls to only add the debug_toolbar urls when it's in the INSTALLED_APPS.

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("__debug__/", include("debug_toolbar.urls")),
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if "debug_toolbar" in settings.INSTALLED_APPS:
+    urlpatterns = path("__debug__/", include("debug_toolbar.urls")) + urlpatterns
